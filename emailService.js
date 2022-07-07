c7a1c83862972b6f7c5160365e4ad38335e519da
Monique Thomas <78518754+MsThomas6@users.noreply.github.com>
2022-07-07 12:24:17 -0700

    Update emailService.js

--- emailService.js
+++ emailService.js
@@ -1,13 +1,11 @@
 import axios from "axios";
-import {API_HOST_PREFIX} from "./serviceHelpers"
 
-const emailEndpoint = `${API_HOST_PREFIX}/api/emails` 
-// const endpoint = "https://localhost:50001/api/emails"
+
+
 const addEmail = (payload) => {
 
   const config = {
     method: "POST",
-    url: emailEndpoint + "/contactus",
     data: payload,
     withCredentials: true,
     crossdomain: true,
